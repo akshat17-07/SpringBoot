9b99049d193c65f7898c58da42d68c3edb37db6a
Akshat Harlalka <akshatharlalka.ah@icloud.com>
2024-12-20 18:24:18 -0500

    Created a dummy request mapping

---
+++ LearnSpringBoot2/LearnSpringBoot2/src/main/java/com/LearnSpringBoot2/LearnSpringBoot2/CourseController.java
@@ -0,0 +1,19 @@
+package com.LearnSpringBoot2.LearnSpringBoot2;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "by Akshat"),
+				new Course(2, "Learn DevOps", "by Akshat")
+				);
+	}
+}
