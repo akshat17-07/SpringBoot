be89f9163c7bc6aad10677ab6046bd7fd6feb3a3
Akshat Harlalka <akshatharlalka.ah@icloud.com>
2024-12-20 18:05:43 -0500

    Started 2nd follow along project

---
+++ LearnSpringBoot2/src/main/java/com/LearnSpringBoot2/LearnSpringBoot2/LearnSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.LearnSpringBoot2.LearnSpringBoot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBoot2Application.class, args);
+	}
+
+}
---
+++ LearnSpringBoot2/src/test/java/com/LearnSpringBoot2/LearnSpringBoot2/LearnSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnSpringBoot2.LearnSpringBoot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
